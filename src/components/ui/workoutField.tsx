bf6d99c0ad108a737c5d4bb231befb1ecef6b9e2
Knirkeli <knirkeli88@msn.com>
2025-05-02 13:41:41 +0200

    Split into new components

---
+++ src/components/ui/workoutField.tsx
@@ -0,0 +1,26 @@
+"use client"
+interface SelectFieldProps {
+    label: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    options: { value: string; label: string }[];
+    required?: boolean;
+  }
+  
+  export default function SelectField({ label, value, onChange, options, required }: SelectFieldProps) {
+    return (
+      <label className="flex flex-col gap-2 font-medium">
+        {label}
+        <select
+          value={value}
+          onChange={onChange}
+          required={required}
+          className="border rounded px-3 py-2 text-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-background text-foreground"
+        >
+          {options.map(opt => (
+            <option key={opt.value} value={opt.value}>{opt.label}</option>
+          ))}
+        </select>
+      </label>
+    );
+  }
---
+++ src/utils/calculateWetBulbTemp.tsx
@@ -0,0 +1,9 @@
+export function calculateWetBulbTemp(temp: number, hum: number, wind: number) {
+    const wetBulbTemp =
+      temp * Math.atan(0.151977 * Math.pow(hum + 8.313659, 0.5)) +
+      Math.atan(temp + hum) - Math.atan(hum - 1.676331) +
+      0.00391838 * Math.pow(hum, 1.5) * Math.atan(0.023101 * hum) - 4.686035;
+  
+    const adjustedWetBulbTemp = wetBulbTemp - (wind * (1 - hum / 100) * 0.07);
+    return adjustedWetBulbTemp;
+  }

5fc1e24bd600ff043632f02879e9aa177605dbd4
Knirkeli <knirkeli88@msn.com>
2025-05-02 20:28:33 +0200

    Lagt til valg for diett.

---
+++ src/constants/dietWaterGains.tsx
@@ -0,0 +1,8 @@
+const DIET_WATER_GAIN = {
+    carbohydrate: 0.5, // liters
+    protein: 0.3,      // liters
+    fruit: 1,          // liters
+    normal: 0.7,       // liters
+  };
+  
+  export default DIET_WATER_GAIN;

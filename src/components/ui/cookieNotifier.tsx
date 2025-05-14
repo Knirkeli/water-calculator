76238d7f8822a9d0f4d7b7b720a2a0dc6ab51f18
Knirkeli <knirkeli88@msn.com>
2025-05-14 09:22:55 +0200

    lagt til cooki

--- src/components/ui/cookieNotifier.tsx
+++ src/components/ui/cookieNotifier.tsx
@@ -82,6 +82,9 @@ export default function CookieNotifier({ onConsentChange }: { onConsentChange?: 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black/50 bg-blur-sm z-50 flex-col gap-4">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-md border border-gray-300">
+        <div>
+            <img src="\Cookie.png" alt="cookie bathing in a galss of water" />
+        </div>
         <div className="mb-2 text-center text-xl text-gray-800">
           <p>We are using cookies and location data to let you fill in weather data with a button click. This is activated by the Auto-fill button.</p> <br />
           <h3 className="font-bold text-2xl">Allow cookies?</h3>
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
-        <header className="fixed top-0 left-0 flex items-center justify-between w-full mx-auto p-4 bg-white shadow-md rounded-b-lg">
+        <header className="fixed top-0 left-0 flex items-center justify-between w-full mx-auto p-4 bg-blue-950 shadow-xl rounded-b-lg">
             <img src="/Vannlogo.png" alt="Water calculator logo" className="h-16 ml-6" />
-            <h1 className="text-2xl font-bold text-gray-800">Water Intake Calculator</h1>
+            <h1 className="text-2xl font-bold text-white">Water Intake Calculator</h1>
             <nav>
                 <p className="text-xl font-bold text-blue-800 mr-6">Stay hydrated!</p>
 

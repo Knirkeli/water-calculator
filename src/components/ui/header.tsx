1ce459b6935bdfc4a6898d5cc71ff94da77977c0
Knirkeli <knirkeli88@msn.com>
2025-05-14 09:30:26 +0200

    Bg endret på header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
-        <header className="fixed top-0 left-0 flex items-center justify-between w-full mx-auto p-4 bg-blue-950 shadow-xl rounded-b-lg">
-            <img src="/Vannlogo.png" alt="Water calculator logo" className="h-16 ml-6" />
-            <h1 className="text-2xl font-bold text-white">Water Intake Calculator</h1>
+        <header className="fixed top-0 left-0 flex items-center justify-between w-full mx-auto p-4 bg-slate-900 shadow-xl rounded-b-lg">
+            <img src="/Vannlogo.png" alt="Water calculator logo" className="h-16 ml-6 z-10" />
+            <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl font-bold text-white whitespace-nowrap">Water Intake Calculator</h1>
             <nav>
                 <p className="text-xl font-bold text-blue-800 mr-6">Stay hydrated!</p>
 
